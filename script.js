c8af1ab6a644bd43f3e5fe691e26814ffb406298
aish <aishnikam31@gmail.com>
2024-02-08 12:40:51 +0530

    not copy if empty

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function copyText(params) {
     var copyTextArea = document.getElementById('copyTextArea');
-    copyTextArea.select();
-    // above code selects the text area
-
-    document.execCommand('copy');
-    // above code copies the selected text, execCommand is a built-in function in JavaScript that is used to copy the selected text 
-
-
-    updateCopyButtonText();
-    // above code updates the button text
+ 
+    if (copyTextArea.value === '') {
+        alert('Please enter some text to copy');
+    }
+    else{
+        copyTextArea.select();
+        // above code selects the text area
+    
+        document.execCommand('copy');
+        // above code copies the selected text, execCommand is a built-in function in JavaScript that is used to copy the selected text 
+    
+    
+        updateCopyButtonText(); // function is hoisted, so it can be called before it is defined
+         
+        // above code updates the button text
+    }
+   
 }
 
 function updateCopyButtonText() {
-    var copyButton = document.querySelector('#copyTextArea + button');
+    var copyButton = document.querySelector('button');
     copyButton.innerText = 'Copied';
     setTimeout(() => {
         copyButton.innerText = 'Copy Text';
